20f85a3475cab83fa2ea2fd74b21b4dfbed1041b
Cassandra Rusk <149620042+C-Rusk@users.noreply.github.com>
2024-03-21 14:24:43 -0500

    JavaScript lesson 4 exercise 1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var footer = document.querySelector(".contact");
+//console.log(footer.outerHTML);
+
+footer.style.backgroundColor = "#d7ddf3";
+
+var tagline = document.querySelector(".tagline");
+tagline.innerText = "Flowers as beautiful as you are.";
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+//console.log(address.innerHTML);
+
+var everyDay = document.querySelector("h3");
+everyDay.innerHTML = "We're here for you <em>every day</em> of the week.";
